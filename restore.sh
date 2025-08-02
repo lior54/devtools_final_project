703cf868d590f3dd4a4d3b7f6d5c570e6b01beb9
Lior Fogel <liorfo57@gmail.com>
2025-08-03 02:29:25 +0300

    added restore.sh

---
+++ restore.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if ! command -v mysqladmin >/dev/null 2>&1; then
+    echo "mysqladmin is not installed, use sudo apt update && sudo apt install MySQL-client-core-8.0"
+    exit 1
+fi
+
+echo "Restoring Joomla database from backup..."
+gunzip < ./my-joomla.backup.sql.gz | docker exec -i joomla_mysql sh -c "exec mysql -h 127.0.0.1 -uroot -pmy-secret-pw --force my-db"
+
+echo "Restarting joomla..."
+docker restart joomla_container
+
+echo "Waiting for Joomla to be healthy..."
+until [ "$(docker inspect --format='{{.State.Health.Status}}' joomla_container)" = "healthy" ]; do
+  sleep 3
+done
+echo "Restore done"
